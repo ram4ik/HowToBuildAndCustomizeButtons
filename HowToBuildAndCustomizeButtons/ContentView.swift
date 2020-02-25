e5e1ba515a301932ed85b31534f18e0de16b6a8e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-25 15:36:33 +0200

    buttons

--- HowToBuildAndCustomizeButtons/ContentView.swift
+++ HowToBuildAndCustomizeButtons/ContentView.swift
@@ -10,7 +10,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            // Normal Button
+            Button(action: { }, label: {
+              Text("This is a button")
+            })
+            
+            // Button Customization
+            Button(action: { }, label: {
+              Text("This is a button")
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(8)
+            })
+            
+            // Shadow
+            .shadow(color: Color.blue, radius: 20, y: 5)
+            
+            // infinity Corner Radius
+            .cornerRadius(.infinity)
+            
+            // Border
+            .padding()
+            .border(Color.blue)
+            
+            // or
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+            
+            // Button With Image
+            Button(action: {}) {
+              HStack{
+                Text("This is a button")
+                .padding(.horizontal)
+                Image(systemName: "person.crop.circle.fill")
+              }
+              .padding()
+            }
+            .foregroundColor(Color.white)
+            .background(Color.blue)
+            .cornerRadius(8)
+            
+            Button(action: {}) {
+              Image("tesla")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 200, height: 50)
+                .cornerRadius(40)
+            }
+        }
     }
 }
 
